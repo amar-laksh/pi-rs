b3fc5ba992e212fd209e83ef79758fa9cf3abdbb
amar-laksh <amar.lakshya@xaviers.edu.in>
2017-09-24 09:04:23 +0530

    ADDED: GITIGNORE, GPIO, COMM, TIME

---
+++ src/gpio.rs
@@ -0,0 +1,7 @@
+use comm::io::*;
+
+
+pub fn gpio_test() {
+    writes("\nStarting GPIO Test\n");
+    writes("\nEnding GPIO Test\n");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,22 @@
 #![no_std]
 #![feature(core_intrinsics, lang_items)]
-
+mod comm;
 use core::intrinsics::abort;
-use core::intrinsics::volatile_load;
-use core::intrinsics::volatile_store;
-
-// raspi2 and raspi3 have peripheral base address 0x3F000000,
-// but raspi1 has peripheral base address 0x20000000. Ensure
-// you are using the correct peripheral address for your
-// hardware.
-const UART_DR: u32 = 0x3F201000;
-const UART_FR: u32 = 0x3F201018;
-
-fn mmio_write(reg: u32, val: u32) {
-    unsafe { volatile_store(reg as *mut u32, val) }
-}
-
-fn mmio_read(reg: u32) -> u32 {
-    unsafe { volatile_load(reg as *const u32) }
-}
-
-fn transmit_fifo_full() -> bool {
-    mmio_read(UART_FR) & (1 << 5) > 0
-}
-
-fn receive_fifo_empty() -> bool {
-    mmio_read(UART_FR) & (1 << 4) > 0
-}
-
-fn writec(c: u8) {
-    while transmit_fifo_full() {}
-    mmio_write(UART_DR, c as u32);
-}
-
-fn getc() -> u8 {
-    while receive_fifo_empty() {}
-    mmio_read(UART_DR) as u8
-}
-
-fn write(msg: &str) {
-    for c in msg.chars() {
-        writec(c as u8);
-    }
-}
+use comm::io::*;
+mod gpio;
+use gpio::*;
 
 #[no_mangle]
+
 pub extern fn kernel_main() {
-    write("Hello Rust Kernel world!");
+    writes("Hello Rust Kernel world!");
+    gpio_test();
     loop {
         let c: u8 = getc();
         if c == '1' as u8 {
-            write("YOU WROTE 1\n");
+            writes("YOU WROTE 1\n");
         } else {
-            write("YOU WROTE SOMETHING ELSE\n");
+            writes("YOU WROTE SOMETHING ELSE\n");
         }
     }
 }
